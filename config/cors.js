fc32829bbab574483719d1c9f2f4f560ea3561b0
mutiu4luv <chidiemmamadu@gmail.com>
2025-09-25 13:28:58 +0100

    cors

---
+++ config/cors.js
@@ -0,0 +1,29 @@
+import cors from "cors";
+
+const rawAllowedOrigins = [
+  process.env.FRONTEND_URL_MAIN,
+  process.env.MAIN_APP_URL,
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+].filter(Boolean);
+console.log(rawAllowedOrigins);
+
+// Normalize (remove trailing slashes)
+const allowedOrigins = rawAllowedOrigins.map((o) => o.replace(/\/$/, ""));
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); // allow Postman / curl
+    const normalizedOrigin = origin.replace(/\/$/, "");
+    if (allowedOrigins.includes(normalizedOrigin)) {
+      callback(null, true);
+    } else {
+      console.warn("❌ Blocked by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+export default cors(corsOptions);
